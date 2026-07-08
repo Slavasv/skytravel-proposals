a7c7d18c37e89cebed9e39f161d1379a7809613d
Slavasv <slavik.sv96@gmail.com>
2026-07-08 14:51:06 +0300

    update debug pdf dovnloater on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  typedRoutes: false,
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
